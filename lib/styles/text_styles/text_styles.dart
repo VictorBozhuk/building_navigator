4d18fe069a0ff852df3f6cb6d3a6c68f5b6a16a5
Victor Bozhuk <bozhook@gmail.com>
2023-04-20 13:42:48 +0300

    deleted global fontFamily

--- lib/styles/text_styles/text_styles.dart
+++ lib/styles/text_styles/text_styles.dart
@@ -1,54 +1,44 @@
 import 'package:flutter/material.dart';
 
 import '../../models/room_model.dart';
-import '../global/global.dart';
 
 const TextStyle textStyleOnSplashScreen = TextStyle(
     color: Colors.white,
-    fontFamily: globalFontFamily,
     fontSize: 40,
     fontWeight: FontWeight.w800);
 
 const TextStyle textStyleCardTitle = TextStyle(
     color: Colors.white,
-    fontFamily: globalFontFamily,
     fontSize: 30,
     fontWeight: FontWeight.w600);
 
 const TextStyle textStyleAppBar = TextStyle(
-    fontFamily: globalFontFamily,
     fontSize: 24,
     fontWeight: FontWeight.w600);
 
 const TextStyle textStyleListOfTitles = TextStyle(
-    fontFamily: globalFontFamily,
     fontSize: 30,
     color: Colors.white,
     fontWeight: FontWeight.w600);
 
 const TextStyle textStyleMainNormalText = TextStyle(
-    fontFamily: globalFontFamily,
     fontSize: 22,
     color: Colors.white);
 
 const TextStyle textStyleMainNormalTextBlack = TextStyle(
-    fontFamily: globalFontFamily,
     fontSize: 22,
     color: Colors.black);
 
 const TextStyle textStyleMainSmallTextBlack = TextStyle(
-    fontFamily: globalFontFamily,
     fontSize: 16,
     color: Colors.black);
 
 const TextStyle textStyleMainNormalDrawerText = TextStyle(
-    fontFamily: globalFontFamily,
     fontSize: 22);
 
 TextStyle textStyleRoomTitleOnPanorama(Room room){
   return TextStyle(
       color: room.color,
-      fontFamily: globalFontFamily,
       fontSize: room.fontSize,
       fontWeight: FontWeight.w700);
 }
